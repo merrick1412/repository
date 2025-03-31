6926f20c78427e47ebe18357aaee4103557724e6
merrick1412 <mwmoncure@gmail.com>
2025-03-30 20:13:10 -0400

    Auto commit: Sun Mar 30 20:13:10 EDT 2025

--- COP4521/module11/forms.py
+++ COP4521/module11/forms.py
@@ -9,3 +9,10 @@ class CustomerForm(FlaskForm):
     security_role_level = IntegerField('Security Role Level', validators=[DataRequired(), NumberRange(min=1, max=3)])
     login_password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Add Customer')
+
+class OrderForm(FlaskForm):
+    item_sku = StringField('Item SKU', validators=[DataRequired(message="Item SKU is required")])
+    quantity = IntegerField('Quantity', validators=[DataRequired(message="Quantity is required"), NumberRange(min=1, message="Quantity must be greater than 0")])
+    price = FloatField('Price', validators=[DataRequired(message="Price is required"), NumberRange(min=0.01, message="Price must be greater than 0")])
+    credit_card = StringField('Credit Card Number', validators=[DataRequired(message="Credit Card number is required")])
+    submit = SubmitField('Add Order')
