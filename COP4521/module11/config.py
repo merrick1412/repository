ab7a7b140e0eb4740e67f86459c40ac1c9df0a94
merrick1412 <mwmoncure@gmail.com>
2025-03-30 20:35:48 -0400

    Auto commit: Sun Mar 30 20:35:48 EDT 2025

--- COP4521/module11/config.py
+++ COP4521/module11/config.py
@@ -4,4 +4,6 @@ class Config:
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"check_same_thread": False}}
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_recycle": 280,
+        "pool_timeout": 60,
+        "pool_size": 10}
