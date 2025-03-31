d19aaf219cdc1994e36923af3ac95db16405a06c
merrick1412 <mwmoncure@gmail.com>
2025-03-30 20:34:22 -0400

    Auto commit: Sun Mar 30 20:34:22 EDT 2025

--- COP4521/module11/config.py
+++ COP4521/module11/config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"check_same_thread": False}}
