76a39d2e644f89d068d73478d6e6f6338478de35
merrick1412 <mwmoncure@gmail.com>
2025-04-06 22:37:29 -0400

    Auto commit: Sun Apr  6 22:37:29 EDT 2025

--- COP4521/module11/models.py
+++ COP4521/module11/models.py
@@ -9,6 +9,19 @@ class Customer(db.Model):
     security_role_level = db.Column(db.Integer, nullable=False)
     login_password = db.Column(db.String(100), nullable=False)
 
+    def set_name(self, name):
+        self.name = encrypt_data(name)
+    def get_name(self):
+        return decrypt_data(self.name)
+    def set_phone_number(self, phone_number):
+        self.phone_number = encrypt_data(phone_number)
+    def get_phone_number(self):
+        return decrypt_data(self.phone_number)
+    def set_login_password(self, password):
+        self.login_password = encrypt_data(password)
+    def get_login_password(self):
+        return decrypt_data(self.login_password)
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
