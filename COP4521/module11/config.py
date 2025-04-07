c2e06251cc0a28aac4490eca505c57212bf5cd4b
merrick1412 <mwmoncure@gmail.com>
2025-04-06 23:26:02 -0400

    Auto commit: Sun Apr  6 23:26:02 EDT 2025

--- COP4521/module11/config.py
+++ COP4521/module11/config.py
@@ -1,9 +1,14 @@
 import os
 
+from cryptography.fernet import Fernet
+
+
 class Config:
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///customers'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_RECYCLE = 300
     SQLALCHEMY_POOL_TIMEOUT = 1
+    encryption_key = b'your_secret_key_here'
+    cipher = Fernet(encryption_key)
 
