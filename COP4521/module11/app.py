8d0242622cba8ef54845cdb9523603c2b8a689b4
merrick1412 <mwmoncure@gmail.com>
2025-04-06 22:36:28 -0400

    Auto commit: Sun Apr  6 22:36:28 EDT 2025

--- COP4521/module11/app.py
+++ COP4521/module11/app.py
@@ -10,7 +10,21 @@ from flask import Flask, render_template, redirect, url_for, request, flash, ses
 from models import db, Customer, Order
 from forms import CustomerForm, OrderForm
 from config import Config
+from cryptography.fernet import Fernet
 import sqlite3
+
+#key
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+def encrypt(message):
+    if message:
+        return cipher_suite.encrypt(message.encode('utf-8'))
+    return None
+def decrypt(encrypted):
+    if encrypted:
+        return cipher_suite.decrypt(encrypted).decode('utf-8')
+    return None
 
 app = Flask(__name__)
 app.config.from_object(Config)
