eec3527412848a365d6099cf9a6ec34e325c7b67
merrick1412 <mwmoncure@gmail.com>
2025-04-06 23:38:46 -0400

    Auto commit: Sun Apr  6 23:38:46 EDT 2025

--- COP4521/module11/models.py
+++ COP4521/module11/models.py
@@ -24,7 +24,7 @@ class Customer(db.Model):
         return decrypt(self.login_password)
 
 class Order(db.Model):
-    __tablename__ = 'CustOrder'
+    __tablename__ = 'Order'
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     item_sku = db.Column(db.String(50), nullable=False)
