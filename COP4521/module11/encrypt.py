fbb1899760d0a93f231ec1ec2a6ac1c8b3294dbb
merrick1412 <mwmoncure@gmail.com>
2025-04-07 22:33:46 -0400

    Auto commit: Mon Apr  7 22:33:46 EDT 2025

--- COP4521/module11/encrypt.py
+++ COP4521/module11/encrypt.py
@@ -1,15 +1,32 @@
-#key
 from cryptography.fernet import Fernet
+import os
 
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
+# Path to key file
+KEY_FILE = 'secret.key'
 
 def generate_key():
     return Fernet.generate_key()
 
+def load_key():
+    if not os.path.exists(KEY_FILE):
+        key = generate_key()
+        with open(KEY_FILE, 'wb') as f:
+            f.write(key)
+    else:
+        with open(KEY_FILE, 'rb') as f:
+            key = f.read()
+    return key
+
+# Load or create key
+key = load_key()
+cipher_suite = Fernet(key)
+
 def encrypt(message):
     if message:
-        return cipher_suite.encrypt(message.encode('utf-8'))
+        return cipher_suite.encrypt(message.encode('utf-8')).decode('utf-8')  # decode so you can store it in DB
     return None
+
 def decrypt(encrypted):
-    return cipher_suite.decrypt(encrypted.encode()).decode('utf-8')
+    if encrypted:
+        return cipher_suite.decrypt(encrypted.encode('utf-8')).decode('utf-8')
+    return None
