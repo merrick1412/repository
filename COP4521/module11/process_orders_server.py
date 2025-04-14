a6e768f4ee820dfc04719f799800e6b080b05b31
merrick1412 <mwmoncure@gmail.com>
2025-04-13 22:33:43 -0400

    Auto commit: Sun Apr 13 22:33:43 EDT 2025

--- COP4521/module11/process_orders_server.py
+++ COP4521/module11/process_orders_server.py
@@ -1,8 +1,14 @@
 import socketserver
 from encrypt import decrypt
 from models import db, Customer, Order
-from app import app
+from flask import Flask
+from config import Config
+from models import db, Customer, Order
+from encrypt import decrypt
 
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 SEPARATOR = "^%$"
 
 class OrderHandler(socketserver.BaseRequestHandler):
