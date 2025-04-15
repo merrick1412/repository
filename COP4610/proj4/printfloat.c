35f7d632ae89b1625db71c765d59eb05f36bed74
merrick1412 <mwmoncure@gmail.com>
2025-04-14 22:02:43 -0400

    Auto commit: Mon Apr 14 22:02:43 EDT 2025

---
+++ COP4610/proj4/printfloat.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+void
+print(float val)
+{
+  //printf(1, "%f\n", val);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+  //if(argc != 2){
+  //  exit();
+  //}
+
+  //print(1.2345);
+  exit();
+}
