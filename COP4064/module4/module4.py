c11a37bf36e500cc6bf5caa1799eb47ba13d6eb0
merrick1412 <mwmoncure@gmail.com>
2025-09-21 14:44:25 -0600

    Auto commit: Sun Sep 21 14:44:25 MDT 2025

---
+++ COP4064/module4/module4.py
@@ -0,0 +1,16 @@
+"""
+Name: Merrick Moncure
+Date: 9/14/2025
+Assignment: Module 3 – Key/Value (pickleDB) Menu App
+Due Date: 9/14/25
+About this project:
+    This project is a menu-driven Python program that lets the user
+    add, edit, and display information about a Laptop.
+    The project demonstrates input validation loops,
+    range checking, and menu control flow.
+Assumptions:
+- Stores one "Laptop" object per day in pickleDB.
+- Key is today's date "YYYY-MM-DD".
+- Implements Add/Edit/Display/Delete with input-validation loops.
+All work below was performed by Merrick Moncure
+"""
