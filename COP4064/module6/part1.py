64665f6f1eb3b15cd30ab392ed7c0bba0f96fc7f
merrick1412 <mwmoncure@gmail.com>
2025-10-05 19:25:14 -0600

    Auto commit: Sun Oct  5 19:25:14 MDT 2025

---
+++ COP4064/module6/part1.py
@@ -0,0 +1,11 @@
+"""
+Name: Merrick Moncure
+Date: 10/05/2025
+Assignment: Document DB Part 1 - Creation and Inserts
+Due Date: 10/05/2025
+About this project:
+- Connects to local MongoDB
+- Drops and recreates a database and collection
+- Inserts 10+ items that satisfy attribute constraints
+- Displays all items using find()
+"""
